7081d5536fe40407c14f2a2e384ed74848ee22c4
SmartGreenOasis <smartgreenoasis@gmail.com>
2025-01-09 22:07:41 +0100

    UPDATE URL

--- build/assets/js/ad255f7e.2e3dacd0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksmart_green_oasis=self.webpackChunksmart_green_oasis||[]).push([[1710],{9987:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Documentation/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);

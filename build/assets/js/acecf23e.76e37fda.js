2ff3b66287030c4ac7d3482d594ece4c4fb9daef
SmartGreenOasis <smartgreenoasis@gmail.com>
2025-01-09 22:11:09 +0100

    UPDATE 2

---
+++ build/assets/js/acecf23e.76e37fda.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksmart_green_oasis=self.webpackChunksmart_green_oasis||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/Documentation/blog","blogTitle":"Blog","authorsListPath":"/Documentation/blog/authors"}')}}]);
